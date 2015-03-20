71efc7689e72e435e199dce40c5a07c57e36e098
John Holmes <jsholmes@alum.mit.edu>
2015-03-20 16:56:32 -0400

    use agnoster prompt for a while -- like powerline

--- modules/jsholmes/init.zsh
+++ modules/jsholmes/init.zsh
@@ -70,3 +70,22 @@ pless() {
         pygmentize -f terminal256 -g -P style=monokai $* | less -FiXRM -x4
     fi
 }
+
+# function to get the short version of the current path
+# requires PWD_LENGTH variable to be set
+# defaults to 30, if not set
+function get_short_path() {
+  HOME_TIDLE="\/Users\/guy"
+    LONG_PATH=`pwd | sed -e "s/$HOME_TIDLE/\~/"`
+
+    # check to see if the prompt path length has been specified
+    if [ ! -n "$PWD_LENGTH" ]; then
+        export PWD_LENGTH=15
+    fi
+
+    if [ ${#LONG_PATH} -gt $PWD_LENGTH ]; then
+            echo "...${LONG_PATH: -$PWD_LENGTH}"
+    else
+            echo $LONG_PATH
+    fi
+}
