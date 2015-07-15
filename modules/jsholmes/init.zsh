f1c7b32b6462aed19cdc911a36b52d559047eb1b
John Holmes <jsholmes@alum.mit.edu>
2015-07-15 09:59:05 -0400

    add the side prompt stuff back in

--- modules/jsholmes/init.zsh
+++ modules/jsholmes/init.zsh
@@ -33,14 +33,15 @@ export EDITOR=vim
 #bindkey '^h' backward-delete-char
 #bindkey '^w' backward-kill-word
 
-#function zle-line-init zle-keymap-select {
-    #VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
-    #RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
-    #zle reset-prompt
-#}
+setopt prompt_subst
+function zle-line-init zle-keymap-select {
+    VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
+    RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
+    zle reset-prompt
+}
 
-#zle -N zle-line-init
-#zle -N zle-keymap-select
+zle -N zle-line-init
+zle -N zle-keymap-select
 export KEYTIMEOUT=1
 
 # edit the command line in vim when i hit v
